a3a7d1bccb070f549d194b115604e8be35c88c24
sam <suntianxin52@sina.com>
2023-08-16 05:28:17 +0800

    sammuny

---
+++ src/components/CommonComponents/web-tablebar/components/breadcrumb.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tabbar-left">
+    <el-icon>
+      <Fold/>
+    </el-icon>
+    <el-breadcrumb separator-icon="ArrowRight">
+      <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+      <el-breadcrumb-item>promotion management</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumb'
+}
+</script>
+
+<style lang="less" scoped>
+.tabbar-left {
+  display: flex;
+  height: 100%;
+  align-items: center;
+
+  .el-icon {
+    margin-right: 10px;
+  }
+}
+</style>
--- src/components/CommonComponents/web-tablebar/tablebar-index.vue
+++ src/components/CommonComponents/web-tablebar/tablebar-index.vue
@@ -1,38 +1,17 @@
 <template>
   <div class="tabbar-container">
-    <div class="tabbar-left">
-      <el-icon>
-        <Fold/>
-      </el-icon>
-      <el-breadcrumb separator-icon="ArrowRight">
-        <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-        <el-breadcrumb-item>promotion management</el-breadcrumb-item>
-      </el-breadcrumb>
+    <div>
+      <Breadcrumb />
     </div>
-    <div class="tabbar-right">
-      <el-button icon="Refresh" circle/>
-      <el-button icon="FullScreen" circle/>
-      <el-button icon="Setting" circle/>
-      <img src="../../../assets/logo.png" alt=""/>
-      <!-- 下拉菜单 -->
-      <el-dropdown>
-    <span class="el-dropdown-link">
-      admin
-      <el-icon class="el-icon--right">
-        <arrow-down/>
-      </el-icon>
-    </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+    <div>
+      <Settings />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import Breadcrumb from './components/breadcrumb.vue'
+import Settings from './components/settings.vue'
 
 </script>
 
@@ -42,26 +21,5 @@
   width: 100%;
   display: flex;
   justify-content: space-between;
-
-  .tabbar-left {
-    display: flex;
-    align-items: center;
-
-    .el-icon {
-      margin-right: 10px;
-    }
-  }
-
-  .tabbar-right {
-    display: flex;
-    align-items: center;
-    img {
-      width: 24px;
-      height: 24px;
-      border: 1px solid #333;
-      border-radius: 50%;
-      margin: 0 10px;
-    }
-  }
 }
 </style>
